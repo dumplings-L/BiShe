5ed31558c5fd5c778b0a833884822178ae1d7d02
Administrator <gdf>
2023-03-07 08:48:00 +0800

    实体，session,swagger

--- src/main/java/personal/xjl/jerrymouse/controller/StudentController.java
+++ src/main/java/personal/xjl/jerrymouse/controller/StudentController.java
@@ -1,6 +1,8 @@
 package personal.xjl.jerrymouse.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import personal.xjl.jerrymouse.entity.Product;
+import personal.xjl.jerrymouse.entity.Student;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +21,13 @@ import java.util.List;
 @Controller
 //如何访问控制器，像servlet的url_pattern
 @RequestMapping("/Student")
+@Api(value = "this is a  student api",tags = "students api")
 public class StudentController {
     //select,list,分层的请求形式
     @RequestMapping("list.do")
     //响应给用户的是html中的body部分
     @ResponseBody
+    @ApiOperation(value = "listStudents",notes = "list methods,显示学生",tags="list Students")
     public String list(){
         //返回字符串list students给用户
         return "list students";
@@ -47,5 +53,14 @@ public class StudentController {
     @ResponseBody
     public String login(String username,@RequestParam("pwd") String password){
         return "welcome you!"+username+",your password is  "+password;
+    }
+    @RequestMapping("getPage.do")
+    public String getPage(String page){
+        return page;
+    }
+    @RequestMapping("add.do")
+    public String add(Student student, Model model, HttpSession session){
+        session.setAttribute("username",student.getName());
+        return "testSession";
     }
 }
---
+++ src/main/java/personal/xjl/jerrymouse/entity/Student.java
@@ -0,0 +1,12 @@
+package personal.xjl.jerrymouse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Student {
+    private String name;
+    private String pwd;
+    private int age;
+}
